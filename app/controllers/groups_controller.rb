be565217e1f10d9cb07488989b00065e6ec7d6fd
拓人馬場 <patashu0919@gmail.com>
2017-01-28 16:01:22 +0900

    making group function

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,17 @@
+class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(create_params)
+    redirect_to :controller => 'chats', :action => 'index'
+  end
+
+  private
+  def create_params
+    params.permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'chats#index'
+  get 'groups/new'    =>     'groups#new'
+  post 'groups'        =>     'groups#create'
 end

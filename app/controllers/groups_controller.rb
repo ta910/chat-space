78eaeb9d9046a4ae95119fff734a72f7f5b4c917
拓人馬場 <patashu0919@gmail.com>
2017-01-28 18:28:55 +0900

    adding member

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
 
   private
   def create_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, user_ids: [])
   end
 
 end

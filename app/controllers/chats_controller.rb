2980cf7307f34fafd07762b57feef550b4690e75
拓人馬場 <patashu0919@gmail.com>
2017-02-01 00:33:00 +0900

    correct

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,13 +2,14 @@ class ChatsController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
-    @chats = Chat.includes(:group).order("created_at DESC")
+    @chats = @group.chats.order("created_at DESC")
     @chat = Chat.new
   end
 
   def create
     @chat = Chat.create(chat_params)
     if @chat.save
+      flash[:notice] = "メッセージが送信されました。"
     else
       flash[:alert] = "メッセージが入力されていません。"
     end

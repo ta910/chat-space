b97e13eee8b1f6cc2086740942e9360f0864675b
拓人馬場 <patashu0919@gmail.com>
2017-02-06 01:27:42 +0900

    correct first

--- spec/controllers/chats_controller_spec.rb
+++ spec/controllers/chats_controller_spec.rb
@@ -4,7 +4,6 @@ require 'devise'
 describe ChatsController do
   let(:user) { create(:user) }
   let(:group) { create(:group) }
-  let(:groups) { create(:group) }
   let(:chat) { create(:chat, user_id: user.id, group_id: group.id) }
   let(:chats) { create_list(:chat, 5, user: user, group: group )}
 
@@ -28,14 +27,13 @@ describe ChatsController do
   end
 
   describe 'POST #create' do
-    let(:request) { post :create, group_id: group, message: attributes_for(:chat) }
 
     it "saves the new message in the database" do
       expect(chat).to be_valid
     end
 
     it "does not save the new contact in the database" do
-     chat = Chat.new(body: nil)
+      chat = build(:chat, body: nil)
       chat.valid?
       expect(chat.errors[:body]).to include("を入力してください")
     end
--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
   factory :chat do
     body                  "takuto"
     image                 "ttt"
-    user_id               "1"
-    group_id              "1"
+    user_id               1
+    group_id              1
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 

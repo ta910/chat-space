1d8ddddd019520fffd99d5e6a8a83e4c1755faa1
拓人馬場 <patashu0919@gmail.com>
2017-02-26 14:55:48 +0000

    correct

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,25 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
   case Rails.env
     when 'production'
+      config.storage = :fog
       config.fog_directory = 's3user--backet'
-      config.asset_host = 's3user--backet.s3-website-ap-northeast-1.amazonaws.com/s3user--backet'
+      config.asset_host = 'https://s3.amazonaws.com/s3user--backet'
 
     when 'development'
+      config.storage = :fog
       config.fog_directory = 's3user--backet'
-      config.asset_host = 's3user--backet.s3-website-ap-northeast-1.amazonaws.com/s3user--backet'
+      config.asset_host = 'https://s3.amazonaws.com/s3user--backet'
 
     when 'test'
+      config.storage = :file
       config.fog_directory = 's3user--backet'
-      config.asset_host = 's3user--backet.s3-website-ap-northeast-1.amazonaws.com/s3user--backet'
+      config.asset_host = 'https://s3.amazonaws.com/s3user--backet'
   end
 end

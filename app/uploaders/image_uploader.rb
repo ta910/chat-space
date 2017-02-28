38411bc347a62515b515acf026cfecb7abfe5901
拓人馬場 <patashu0919@gmail.com>
2017-02-28 11:24:04 +0000

    installing rmagick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
+  process :resize_to_limit => [300, 300]
 
   # Choose what kind of storage to use for this uploader:
   # storage :fog

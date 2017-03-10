a042c9006dff1d71d3b51f7f2ee2308898b11443
拓人馬場 <patashu0919@gmail.com>
2017-03-10 20:12:25 +0900

    correct

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.7.2'
 
 set :application, 'chat-space'
-set :repo_url, 'git@github.com:chat-space.git'
+set :repo_url, 'git@github.com:ta910/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['chatspace-key.pem']
+                  keys: ['/Users/takutobaba/.ssh/chatspace-key.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }

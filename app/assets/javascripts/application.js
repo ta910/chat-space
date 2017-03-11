3ae7faab8b1f84d786441f6e0f773de158dd35ea
拓人馬場 <patashu0919@gmail.com>
2017-03-11 21:41:49 +0900

    try js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-// = require turbolinks
+//= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
